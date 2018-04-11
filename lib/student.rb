e7530ff4ecb990a294c7626ea8fce39f3d5d35bf
Ahamed <ahamedali95@gmail.com>
2018-04-11 09:28:48 -0400

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -30,8 +30,9 @@ class Student
 
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) VALUES (? : ?)
-      SQL
+      INSERT INTO students (name, grade)
+      VALUES (?, ?)
+    SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
 
@@ -43,4 +44,5 @@ class Student
     student.save
     student
   end
+
 end
